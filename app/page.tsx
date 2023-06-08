8020ff35373485fd3490b8d67b46cc7fc77687a7
Arik Cohen <me@arikcohen.com>
2023-06-08 13:11:14 -0400

    better

--- app/page.tsx
+++ app/page.tsx
@@ -9,19 +9,25 @@ const lexdec = Lexend_Deca({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="grid place-content-center min-h-screen">
-      <div className="flex flex-col lg:flex-row-reverse max-w-5xl rounded-lg overflow-hidden m-10">
+      <div className="flex flex-col md:flex-row-reverse max-w-5xl rounded-lg overflow-hidden m-10">
         <div className="relative w-full">
-          <div className="relative aspect-video lg:aspect-square">
+          <div className="relative aspect-video md:aspect-square">
+            <Image
+              src={banner}
+              alt="banner image"
+              fill={true}
+              className="object-cover md:hidden"
+            />
             <Image
               src={bannerDesktop}
               alt="banner image"
               fill={true}
-              className="object-cover"
+              className="object-cover hidden md:block"
             />
           </div>
           <div className="bg-primary-4 w-full h-full left-0 top-0 absolute z-10 opacity-50"></div>
         </div>
-        <div className="bg-primary-2 py-16 pl-16 pr-28 gap-8 grid">
+        <div className="bg-primary-2 p-16 gap-8 grid text-center md:text-left">
           <h1 className="text-4xl font-semibold text-neutral-1">
             Get <span className="text-primary-3">insights</span> that help your
             business grow.
@@ -30,7 +36,7 @@ export default function Home() {
             Discover the benefits of data analytics and make better decisions
             regarding revenue, customer experience, and overall efficiency.
           </p>
-          <div className="flex justify-between self-end">
+          <div className="flex justify-between flex-col gap-8 md:flex-row self-end">
             <div>
               <h2 className="text-2xl font-semibold text-neutral-1">10k+</h2>
               <p
